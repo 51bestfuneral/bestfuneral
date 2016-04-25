94baced4ef2565f4720c237b7e5762f405e05fd4
li.yuan <li.yuan@51bestfuneral.com>
2016-04-26 04:52:35 +0800

    commit;

--- src/main/java/com/funeral/kris/service/ExpressServiceImpl.java
+++ src/main/java/com/funeral/kris/service/ExpressServiceImpl.java
@@ -1,60 +1,168 @@
 package com.funeral.kris.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 @Service
 @Transactional
 public class ExpressServiceImpl implements ExpressService{
 
 	public static final String express_key = "29833628d495d7a5";
+	
+	private String EBusinessID="1256666";	
+
+	private String AppKey="bc398cb6-30ee-4f87-bde8-3b15f8c595f8";	
+
+	private String ReqURL="http://api.kdniao.cc/Ebusiness/EbusinessOrderHandle.aspx";	
 
 	@SuppressWarnings("static-access")
-	public String searchExpress(String expressNo,String expressCompany) {
-		try {
-			String address="http://api.kuaidi100.com/api?id="+express_key+"&com="+expressCompany+"&nu="+expressNo+"&show=0&muti=1&order=desc";
-			URL url = new URL(address);
-			URLConnection con = url.openConnection();
-			con.setAllowUserInteraction(false);
-			InputStream urlStream = url.openStream();
-			String type = con.guessContentTypeFromStream(urlStream);
-			String charSet = null;
-			if (type == null)
-				type = con.getContentType();
+	public String searchExpress(String orderNo,String expressNo,String expressCompanyCode) throws Exception{
+		String requestData= "{'OrderCode':'"+expressNo+"','ShipperCode':'"+expressCompanyCode+"','LogisticCode':'"+expressNo+"'}";
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("RequestData", urlEncoder(requestData, "UTF-8"));
+		params.put("EBusinessID", EBusinessID);
+		params.put("RequestType", "1002");
+		String dataSign=encrypt(requestData, AppKey, "UTF-8");
+		params.put("DataSign", urlEncoder(dataSign, "UTF-8"));
+		params.put("DataType", "2");
+		
+		String result=sendPost(ReqURL, params);	
+		
+		return result;
+	}
+	
+	public String getOrderTracesByXml() throws Exception{
+		String requestData= "<?xml version=\"1.0\" encoding=\"utf-8\" ?>"+
+							"<Content>"+
+							"<OrderCode></OrderCode>"+
+							"<ShipperCode>SF</ShipperCode>"+
+							"<LogisticCode>589707398027</LogisticCode>"+
+							"</Content>";
+		
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("RequestData", urlEncoder(requestData, "UTF-8"));
+		params.put("EBusinessID", EBusinessID);
+		params.put("RequestType", "1002");
+		String dataSign=encrypt(requestData, AppKey, "UTF-8");
+		params.put("DataSign", urlEncoder(dataSign, "UTF-8"));
+		params.put("DataType", "1");
+		
+		String result=sendPost(ReqURL, params);	
+		return result;
+	}
+ 
+	private String MD5(String str, String charset) throws Exception {
+	    MessageDigest md = MessageDigest.getInstance("MD5");
+	    md.update(str.getBytes(charset));
+	    byte[] result = md.digest();
+	    StringBuffer sb = new StringBuffer(32);
+	    for (int i = 0; i < result.length; i++) {
+	        int val = result[i] & 0xff;
+	        if (val <= 0xf) {
+	            sb.append("0");
+	        }
+	        sb.append(Integer.toHexString(val));
+	    }
+	    return sb.toString().toLowerCase();
+	}
+	
+	private String base64(String str, String charset) throws UnsupportedEncodingException{
+		String encoded = Base64.encode(str.getBytes(charset));
+		return encoded;    
+	}	
 
-			if (type == null || type.trim().length() == 0 || type.trim().indexOf("text/html") < 0)
-				return null;
-
-			if (type.indexOf("charset=") > 0)
-				charSet = type.substring(type.indexOf("charset=") + 8);
-
-			byte b[] = new byte[10000];
-			int numRead = urlStream.read(b);
-			String content = new String(b, 0, numRead);
-			while (numRead != -1) {
-				numRead = urlStream.read(b);
-				if (numRead != -1) {
-					// String newContent = new String(b, 0, numRead);
-					String newContent = new String(b, 0, numRead, charSet);
-					content += newContent;
-				}
-			}
-			// return content;
-			System.out.println("content:" + content);
-			urlStream.close();
-			return content;
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+	private String urlEncoder(String str, String charset) throws UnsupportedEncodingException{
+		String result = URLEncoder.encode(str, charset);
+		return result;
+	}
+	
+	private String encrypt (String content, String keyValue, String charset) throws UnsupportedEncodingException, Exception
+	{
+		if (keyValue != null)
+		{
+			return base64(MD5(content + keyValue, charset), charset);
 		}
-		return null;
+		return base64(MD5(content, charset), charset);
 	}
 
+	private String sendPost(String url, Map<String, String> params) {
+        OutputStreamWriter out = null;
+        BufferedReader in = null;        
+        StringBuilder result = new StringBuilder(); 
+        try {
+            URL realUrl = new URL(url);
+            HttpURLConnection conn =(HttpURLConnection) realUrl.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            
+            conn.setRequestMethod("POST");
+            
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.connect();
+            
+            out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+                       
+            if (params != null) {
+		          StringBuilder param = new StringBuilder(); 
+		          for (Map.Entry<String, String> entry : params.entrySet()) {
+		        	  if(param.length()>0){
+		        		  param.append("&");
+		        	  }	        	  
+		        	  param.append(entry.getKey());
+		        	  param.append("=");
+		        	  param.append(entry.getValue());		        	  
+		        	  System.out.println(entry.getKey()+":"+entry.getValue());
+		          }
+		          System.out.println("param:"+param.toString());
+		          out.write(param.toString());
+            }
+            
+            out.flush();
+            
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (Exception e) {            
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                if(out!=null){
+                    out.close();
+                }
+                if(in!=null){
+                    in.close();
+                }
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+        return result.toString();
+    }
+	
 }
