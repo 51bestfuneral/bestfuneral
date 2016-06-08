43591bacaec3f46340ab7e30fb944d655161c99d
li.yuan <li.yuan@51bestfuneral.com>
2016-06-08 08:57:14 +0800

    commit

--- src/main/java/com/funeral/kris/dao/WishlistDAO.java
+++ src/main/java/com/funeral/kris/dao/WishlistDAO.java
@@ -1,11 +1,15 @@
 package com.funeral.kris.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import com.funeral.kris.model.ExpressInfo;
 import com.funeral.kris.model.Wishlist;
 
 @Repository
 public interface WishlistDAO extends PagingAndSortingRepository<Wishlist, Integer>{
 
+	public List<Wishlist> findListByUserId(int userId);
 }
