ba00fc2c7e3e1c17c65e4cd039a5c81c6e526e18
li.yuan <li.yuan@51bestfuneral.com>
2016-06-23 21:48:17 +0800

    commit

---
+++ src/main/java/com/funeral/kris/util/PayConfigUtil.java
@@ -0,0 +1,22 @@
+package com.funeral.kris.util;
+
+
+import com.funeral.kris.init.ENV;
+
+public class PayConfigUtil {
+
+	public static String APP_ID = ENV.getValue("APP_ID");
+	
+	public static String MCH_ID = ENV.getValue("MCH_ID");
+	
+	public static String API_KEY = ENV.getValue("API_KEY");
+	
+	public static String CREATE_IP = ENV.getValue("CREATE_IP");
+	
+	public static String NOTIFY_URL = ENV.getValue("NOTIFY_URL");
+	
+	public static String UFDODER_URL = ENV.getValue("UFDODER_URL");
+	
+	
+	
+}
